bf84ac8776853484be983ef62f98742f0df3e013
Tomasz Zdżyłowski <tomasz.zdzylowski@gmail.com>
2020-04-04 02:01:05 +0200

    add atomic design structure

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputComponent } from './atoms/input/input.component';
+import { ButtonComponent } from './atoms/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
