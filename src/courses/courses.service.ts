e424cdd5b22f2dadb675c5de7004c92c5dd9c44c
Jakub Wilk <jw@jakubwilk.pl>
2020-04-14 16:31:05 +0200

    Add cover photo in service method and fix styles on courses section

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -19,6 +19,7 @@ export class CoursesService {
             {
                 id: 1,
                 name: 'WordPress Ninja - Tutorial for beginners!',
+                coverPhoto: 'https://i.imgur.com/fzLDwg0.jpg',
                 rate: 9,
                 author: 'John Krasinsky',
                 category: 'Wordpress'
@@ -26,6 +27,7 @@ export class CoursesService {
             {
                 id: 2,
                 name: 'Java for donkeys',
+                coverPhoto: 'https://i.imgur.com/nxd0nIo.jpg',
                 rate: 9,
                 author: 'Bob Johnson',
                 category: 'Java'
@@ -33,6 +35,7 @@ export class CoursesService {
             {
                 id: 3,
                 name: 'Write own plugins in Wordpress',
+                coverPhoto: 'https://i.imgur.com/mSN464m.png',
                 rate: 8.8,
                 author: 'Andrew Buda',
                 category: 'Wordpress'
@@ -40,6 +43,7 @@ export class CoursesService {
             {
                 id: 4,
                 name: 'Spring is better than cool - Java for true hero',
+                coverPhoto: 'https://i.imgur.com/K5W9ACa.png',
                 rate: 8,
                 author: 'Matt Reves',
                 category: 'Java'
@@ -47,6 +51,7 @@ export class CoursesService {
             {
                 id: 5,
                 name: 'Why you should learn Kotlin?',
+                coverPhoto: 'https://i.imgur.com/zoCoz0U.jpg',
                 rate: 8,
                 author: 'Ash Ketchup',
                 category: 'Android'
@@ -54,6 +59,7 @@ export class CoursesService {
             {
                 id: 6,
                 name: 'Every single web page needs some HTML',
+                coverPhoto: 'https://i.imgur.com/ChToseA.png',
                 rate: 7.9,
                 author: 'Daniel Abrams',
                 category: 'HTML'
@@ -61,6 +67,7 @@ export class CoursesService {
             {
                 id: 7,
                 name: 'NodeJS is everything what you need to be a truly programmer',
+                coverPhoto: 'https://i.imgur.com/RuUqstD.jpg',
                 rate: 7.8,
                 author: 'Jason Born',
                 category: 'JavaScript'
@@ -68,6 +75,7 @@ export class CoursesService {
             {
                 id: 8,
                 name: `Why you shouldn't use Hooks in your app?`,
+                coverPhoto: 'https://i.imgur.com/0s8psfM.jpg',
                 rate: 7.7,
                 author: 'Gaben Potts',
                 category: 'React'
