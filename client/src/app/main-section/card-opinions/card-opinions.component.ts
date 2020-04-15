adb7b52e5d7fe9fc108b8efb886b13332fd4fef9
Tomasz Zdżyłowski <tomasz.zdzylowski@gmail.com>
2020-04-15 11:00:48 +0200

    fix import of service

--- client/src/app/main-section/card-opinions/card-opinions.component.ts
+++ client/src/app/main-section/card-opinions/card-opinions.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpService } from 'src/app/services/http.service';
+import { HttpService } from '../../services/http.service';
 
 @Component({
   selector: 'app-card-opinions',
