4e4ddfbee8734843ebd8c77a84bf0da2ae96acd6
Kamil <kamil.zarebski@onet.pl>
2020-04-29 01:13:32 +0200

    dashboard desktop version

--- client/src/app/otherComponents/dashboard/dashboard.component.ts
+++ client/src/app/otherComponents/dashboard/dashboard.component.ts
@@ -11,5 +11,9 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  tablica = ['html', 'html1', 'html2', 'html3', 'html4',
+    'html5', 'html6', 'html7', 'html8', 'html9']
+  tablicaUser = ['John Snow', 'John Snow1', 'John Snow2',
+    'John Snow3', 'John Snow4', 'John Snow5', 'John Snow6',
+    'John Snow7', 'John Snow8', 'John Snow9']
 }
