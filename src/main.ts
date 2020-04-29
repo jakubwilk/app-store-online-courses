fd20a5b7f99ec2e302fb95bce24937a5813e044b
Jakub Wilk <jw@jakubwilk.pl>
2020-04-29 13:52:02 +0200

    Update CORS and remove statusCode from login method in users controller

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import {ValidationPipe} from '@nestjs/common';
 import {AppModule} from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors:true });
+  const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: '*',
+        credentials: true,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    });
     app.useGlobalPipes(new ValidationPipe({
         validationError: {
             value: false,
