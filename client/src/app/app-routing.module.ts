7aae96b36d5c16b34199b56685729b636560de02
Jakub Wilk <jw@jakubwilk.pl>
2020-04-29 18:20:56 +0200

    PR: Admin Dashboard (#9)
    * add branch and create component for dashboard
    
    * dashboard desktop version
    
    * add fix for dashboard
    
    * Fix styles for dashboard
    
    Co-authored-by: Kamil <kamil.zarebski@onet.pl>

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { FrontPageComponent } from './front-page/front-page.component';
 import { LoginComponent } from './otherComponents/login/login.component';
 import { RegisterComponent } from './otherComponents/register/register.component';
+import { DashboardComponent } from './otherComponents/dashboard/dashboard.component';
 
 
 
 const routes: Routes = [
   {
-    path: 'Home',
+    path: 'home',
     component: FrontPageComponent
   },
   {
@@ -17,15 +18,18 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'Home',
+    redirectTo: 'home',
     pathMatch: 'full',
 
   },
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
   }
-
 ];
 
 @NgModule({
