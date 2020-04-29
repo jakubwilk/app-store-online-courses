59bed93074862154eebc52b202a21759b1eac18c
Tomasz Zdżyłowski <tomasz.zdzylowski@gmail.com>
2020-04-29 17:54:01 +0200

    use reactive form for register

--- client/src/app/otherComponents/login/login.component.ts
+++ client/src/app/otherComponents/login/login.component.ts
@@ -9,7 +9,6 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  loginUserData: Login = {};
   loginForm: FormGroup;
 
   constructor(private login: LoginService, private fb: FormBuilder) { }
--- client/src/app/otherComponents/register/register.component.ts
+++ client/src/app/otherComponents/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RegisterService } from '../../services/register.service';
 import { Register } from '../../interfaces/register';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,22 +9,26 @@ import { Register } from '../../interfaces/register';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
-  registerUserData: Register = {};
 
 
-  constructor(private register: RegisterService) { }
+
+  constructor(private register: RegisterService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      username: '',
+      email: '',
+      password: '',
+      repassword: '',
+      type: ''
+
+    });
+    //this.registerForm.valueChanges.subscribe(console.log);
   }
   onSubmit() {
-    this.registerUser();
-
-  }
-  registerUser() {
-    console.log(this.registerUserData);
-    console.log(this.registerUserData.type);
-    this.register.registerUser(this.registerUserData)
+    this.register.registerUser(this.registerForm.value)
       .subscribe(
         response => {
           console.log(response);
@@ -34,4 +39,9 @@ export class RegisterComponent implements OnInit {
       );
   }
 
+
 }
+
+
+
+
